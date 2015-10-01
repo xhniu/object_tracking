87f60649e8dca781b11a6584a6c3d97b70b89e11
xhniu <653943306@qq.com>
2015-10-01 10:03:21 +0800

    first version

---
+++ object_tracking.h
@@ -0,0 +1,23 @@
+/********************************************************************
+	created:	2015/10/01
+	created:	1:10:2015   9:30
+	filename: 	C:\Users\royal\Documents\Visual Studio 2010\Projects\object_tracking\object_tracking.h
+	file path:	C:\Users\royal\Documents\Visual Studio 2010\Projects\object_tracking
+	file base:	object_tracking
+	file ext:	h
+	author:		xhniu
+	
+	purpose:	
+*********************************************************************/
+#ifndef _OBJECT_TRACKING_H_
+#define _OBJECT_TRACKING_H_
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <stdlib.h>
+
+using namespace std;
+using namespace cv;
+
+#endif
